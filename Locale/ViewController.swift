b3101ab2dbe511b466d796340e9ea49b48cf460f
syavah <syavah@gmail.com>
2017-05-30 23:27:45 +0300

    Initial Commit

---
+++ Locale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locale
+//
+//  Created by Yaroslav Sarnitskiy on 30.05.17.
+//  Copyright © 2017 GS Develop Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

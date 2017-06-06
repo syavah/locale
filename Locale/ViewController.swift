243117d1d66e10efa4b80842e112c80e65fbde53
syavah <syavah@gmail.com>
2017-06-06 22:50:06 +0300

    Full project

--- Locale/ViewController.swift
+++ Locale/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        populateValues()
+    }
+    
+    func populateValues() {
+        var numberFormatter: NumberFormatter {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            return formatter
+        }
+        
+        var currencyFormatter: NumberFormatter {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            return formatter
+        }
+        
+        var dateFormatter: DateFormatter {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .medium
+            formatter.timeStyle = .medium
+            return formatter
+        }
+        
+        textLabel.text = NSLocalizedString("GOOD_MORNING",comment:"Good Morning")
+        numberLabel.text = numberFormatter.string(from: 9999999.999)
+        currencyLabel.text = currencyFormatter.string(from: 5000)
+        dateLabel.text = dateFormatter.string(from: NSDate() as Date)
+        imageView.image = UIImage(named: "\(NSLocalizedString("IMG",comment:"adele-hello"))")
     }
 
     override func didReceiveMemoryWarning() {
